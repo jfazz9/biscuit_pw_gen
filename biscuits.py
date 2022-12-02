f009fdb62d5f2eff62910fd1e2c1967610d50a40
jfazz9 <jftennis@gmail.com>
2022-12-02 12:46:28 +0000

    adding the data files

---
+++ biscuits.py
@@ -0,0 +1,10 @@
+all_biscuits = [
+    'bourbon', 'rich tea', 'ginger-nuts', 'viscount', 'fig rolls', 'digestive',
+    'hobnob', 'nice', 'choc chip cookie', 'malted milk', 'pink wafers', 'jaffa cake',
+    'shortbread', 'choc hobnob', 'choc fingers', 'party rings', 'choc digestive',
+    'jammie dodgers', 'jam and cream', 'white choc fingers', 'viennese', 'custard cream',
+    'tea cakes', 'choc rounds'
+]
+symbols = {'a': '@', 'e': '$', 'i': '!', 'o': '#', 'u': '%'}
+
+all_symbols = '~`! @#$%^&*()_-+={[}]|\:;\"\'<,>.?/'
